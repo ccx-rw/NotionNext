e7b2b8e94ae3b011b5592fd6e8deb2b1db8e26fb
ccx-rw <34044594+ccx-rw@users.noreply.github.com>
2024-05-30 19:56:55 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,8 +19,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'ai-free.club.com',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '',
-  HEO_HERO_TITLE_LINK: '',
+  HEO_HERO_TITLE_5: 'https://blog.ai-free.club',
+  HEO_HERO_TITLE_LINK: 'https://blog.ai-free.club',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -108,7 +108,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: '',
+  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
