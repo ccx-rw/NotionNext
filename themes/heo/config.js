4225e1dad9f0afb67704d093501a7d60ce1c4426
ccx-rw <34044594+ccx-rw@users.noreply.github.com>
2024-06-02 18:50:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.ai-free.club' },
-    { title: '点击获取AI最新资讯', url: 'https://blog.ai-free.club' }
+    { title: '点击获取AI最新资讯', url: 'https://blog.ai-free.club/ai/news' }
   ],
 
   // 英雄区左右侧组件颠倒位置
